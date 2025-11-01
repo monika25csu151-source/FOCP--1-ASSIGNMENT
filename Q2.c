69348e9818352fae321f19bed9047d0fb18aa65c
Monika <monika25csu151@ncuindia.edu>
2025-11-01 11:32:13 +0530

    Fix formatting in Q2.c for Euclidean Algorithm

---
+++ Q2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, temp;
+
+    printf("Enter two integers: ");
+    scanf("%d %d", &a, &b);
+
+    
+    while (b != 0) {
+        temp = b;
+        b = a % b;
+        a = temp;
+    }
+
+    printf("HCF is %d\n", a);
+
+    return 0;
+}
